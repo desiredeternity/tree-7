9b62bb5074dfedc3ae2fcdb62353917ee3ab7b21
desiredeternity <don.bdf2011@mail.ru>
2025-03-13 23:58:32 +0300

    add base for RedBlackTree

---
+++ lib/src/main/kotlin/tree/RedBlackTree.kt
@@ -0,0 +1,19 @@
+class RedBlackTree<K : Comparable<K>, V> : Tree<K, V> {
+    private var root: RedBlackTreeNode<K, V>? = null
+
+    override fun search(key: K): V? {
+        // Реализация поиска
+    }
+
+    override fun insert(key: K, value: V) {
+        // Реализация вставки
+    }
+
+    override fun delete(key: K) {
+        // Реализация удаления
+    }
+
+    override fun iterator(order: TreeTraversalOrder): Iterator<Pair<K, V>> {
+        return TreeIterator(root, order)
+    }
+}
